20e1aedf11747a44548edf6783cc22379990b7cc
slattstoner <alshevsky02@gmail.com>
2026-02-26 19:50:26 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -265,7 +265,7 @@ async def init_driver():
         options.add_argument(f'--proxy-server={PROXY_URL}')
     
     try:
-        driver = uc.Chrome(options=options, version_main=None)  # автоопределение
+driver = uc.Chrome(options=options, version_main=145)
         logger.info("✅ undetected_chromedriver успешно запущен")
     except Exception as e:
         logger.error(f"❌ Ошибка запуска драйвера: {e}")
