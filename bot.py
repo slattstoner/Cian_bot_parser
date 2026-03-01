90164dace29e236472d77d8df64bafd99b3b0b7f
slattstoner <alshevsky02@gmail.com>
2026-03-01 21:21:10 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,8 +2,11 @@
 import asyncio
 import logging
 import signal
+import json
+import time
+import random
+import re
 from telegram.ext import Application, CommandHandler, CallbackQueryHandler, MessageHandler, filters, PreCheckoutQueryHandler, ConversationHandler
-
 from config import TOKEN, PAYMENT_PROVIDER_TOKEN, DATABASE_URL
 from database import Database
 from handlers import (
